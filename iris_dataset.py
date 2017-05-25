e7f2f2ea56c5b1bcbaac26328ed50f706be0e449
eduzen <eduardo.a.enriquez@gmail.com>
2017-05-25 16:20:36 -0300

    first commit

---
+++ iris_dataset.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from scipy import sparse
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from matplotlib.colors import ListedColormap
+
+iris_dataset = load_iris()
+
+X_train, X_test, y_train, y_test = train_test_split(
+    iris_dataset['data'], iris_dataset['target'], random_state=0
+)
+
+iris_dataframe = pd.DataFrame(X_train, columns=iris_dataset.feature_names)
+
+grr = pd.plotting.scatter_matrix(
+    iris_dataframe, c=y_train, figsize=(15, 15), marker='o',
+    hist_kwds={'bins': 20}, s=60, alpha=.8,
+    cmap=ListedColormap(['#0000aa', '#ff2020', '#50ff50'])
+)
+plt.show()
